899928cdb1f264a378bca69e5ba40158c0e75e4a
ijstt <119423325+ijstt@users.noreply.github.com>
2024-02-28 23:53:16 +0300

    Update main.py

--- main.py
+++ main.py
@@ -68,5 +68,78 @@ async def back(callback_query: types.CallbackQuery):
                            reply_markup=kb.txt1)
 
 
+@dp.message_handler(text=['Тест'])
+async def quiz(message: types.Message):
+    id = message.from_user.id
+
+    if int(db.get_ans(id)) == 0:
+        await bot.send_message(id,
+                               text="Вы хотите начать тест? У вас будет 1 попытка",
+                               reply_markup=kb.quiz)
+        await bot.delete_message(message.from_user.id, message.message_id)
+    else:
+        await bot.send_message(id,
+                               text="Вы уже прошли викторину, с вашими баллами вы можете ознакомиться в личном кабинете")
+
+
+@dp.callback_query_handler(lambda x: x.data == "quiz")
+async def quiz(callback_query: types.CallbackQuery):
+    id = callback_query.from_user.id
+
+    data = db.get_ques()
+    num = int(db.get_tb(id))
+
+    for k, v in data[0].items():
+        for j, i in v.items():
+            text = f"{k} {j}"
+    await bot.send_message(id,
+                           text=text,
+                           reply_markup=kb.ans)
+
+    db.set_tb(num + 1, callback_query.from_user.id)
+
+
+@dp.callback_query_handler(lambda x: x.data in "АБВГ")
+async def check_quiz(callback_query: types.CallbackQuery):
+    id = callback_query.from_user.id
+    s = int(db.get_tb(id))
+    data = db.get_ques()
+    ans = callback_query.data
+
+    for k, v in data[s].items():
+        for j, i in v.items():
+            if ans == i:
+                db.set_ans(db.get_ans(id) + 1, id)
+            text = f"{k} {j}"
+
+        if len(data) != s:
+            await bot.send_message(id,
+                                   text=text,
+                                   reply_markup=kb.ans)
+        else:
+            await bot.send_message(id,
+                                   text="Завершить тест",
+                                   reply_markup=kb.end)
+
+    db.set_tb(s + 1, callback_query.from_user.id)
+
+
+@dp.callback_query_handler(lambda x: x.data == "end")
+async def end_que(callback_query: types.CallbackQuery):
+    id = callback_query.from_user.id
+    current_ball = db.get_ans(id)
+    if current_ball * 100 // 8 >= 66:
+        await bot.send_message(callback_query.from_user.id,
+                               text="Поздравляем! У вас отличный результат")
+    elif 40 <= current_ball * 100 // 8 < 66:
+        await bot.send_message(callback_query.from_user.id,
+                               text="Неплохой результат, но еще есть к чему стремиться :)")
+    else:
+        await bot.send_message(callback_query.from_user.id,
+                               text="Советуем вам зайди в раздел справочника и изучить теорию по ПДД")
+    await bot.send_message(callback_query.from_user.id,
+                           text=f"Вы набрали - {current_ball} из 8 баллов")
+
+
 if __name__ == '__main__':
     executor.start_polling(dp, skip_updates=True)
