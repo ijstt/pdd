60a6e8fd2bb56ca6bfee0b2d9465cbdea33bf7da
ijstt <119423325+ijstt@users.noreply.github.com>
2024-02-28 23:53:30 +0300

    Update keyboard.py

--- keyboard.py
+++ keyboard.py
@@ -13,3 +13,18 @@ txt1.add(next)
 txt2 = types.InlineKeyboardMarkup(row_width=1)
 back = types.InlineKeyboardButton("Назад", callback_data="back")
 txt2.add(back)
+
+quiz = types.InlineKeyboardMarkup(row_width=1)
+yes = types.InlineKeyboardButton("Продолжить", callback_data="quiz")
+quiz.add(yes)
+
+ans = types.InlineKeyboardMarkup(row_width=4)
+a = types.InlineKeyboardButton("А", callback_data="А")
+b = types.InlineKeyboardButton("Б", callback_data="Б")
+v = types.InlineKeyboardButton("В", callback_data="В")
+g = types.InlineKeyboardButton("Г", callback_data="Г")
+ans.add(a, b, v, g)
+
+end = types.InlineKeyboardMarkup()
+btn = types.InlineKeyboardButton("Завершить викторину", callback_data="end")
+end.add(btn)
